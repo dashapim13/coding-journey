bae395e02cd096f4b1d8c32b26f42e420e72e561
Daria Borisenko <dashapim@gmail.com>
2024-11-18 15:25:47 -0500

    Deleted, added

---
+++ SolutionForProjects/ConsoleAppPractice/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleAppPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter UserName: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
+
+        }
+    }
+}
--- SolutionForProjects/Homework/Program.cs
+++ SolutionForProjects/Homework/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace FirstProject
+﻿namespace Homework
+
 {
     internal class Program
     {
