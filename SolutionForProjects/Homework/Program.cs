32280b1be9494187b601dafd7a55f6f57d701da9
Daria Borisenko <dashapim@gmail.com>
2024-11-18 16:03:18 -0500

    Method to check if string is a palindrome

--- SolutionForProjects/Homework/Program.cs
+++ SolutionForProjects/Homework/Program.cs
@@ -3,10 +3,38 @@
 {
     internal class Program
     {
+
+        public static bool IsPalindrome(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;    //Return false or null or empty strings
+            }
+
+            //Remove whitespace and convert to lowercase
+            string cleanedInput = input.Replace(" ", "").ToLower();
+
+            //Compare the original cleaned input with its reverse
+            char[] charArray = cleanedInput.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedInput = new string(charArray);
+            return cleanedInput == reversedInput;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a string to check if it's a pallindrome:");
+            string userInput = Console.ReadLine();
 
+            if (IsPalindrome(userInput))
+            {
+                Console.WriteLine("The string is a pallindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The string is not a pallindrome.");
+
+            }
         }
     }
 }
